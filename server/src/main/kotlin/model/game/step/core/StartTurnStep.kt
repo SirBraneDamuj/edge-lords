0e4fdfa542570b124c6bb581205f57710a994726
Zach Thacker <zpthacker@gmail.com>
2021-02-20 16:37:30 -0500

    add autoheal

--- server/src/main/kotlin/model/game/step/core/StartTurnStep.kt
+++ server/src/main/kotlin/model/game/step/core/StartTurnStep.kt
@@ -1,18 +1,27 @@
 package model.game.step.core
 
+import model.AUTO_HEALERS
 import model.game.Game
 import model.game.PlayerLabel
 import model.game.Position
 import model.game.step.GameStep
+import model.game.step.effects.HealCreatureStep
 
 class StartTurnStep(
     val playerLabel: PlayerLabel
 ) : GameStep {
     override fun perform(game: Game): List<GameStep> {
-        return listOf(
-            DrawCardStep(playerLabel),
-            ReadyCreaturesStep(playerLabel, Position.allPositions),
-            IncrementAndRestoreManaStep(playerLabel)
-        )
+        val player = game.player(playerLabel)
+
+        return mutableListOf<GameStep>().apply {
+            this.add(DrawCardStep(playerLabel))
+            this.add(ReadyCreaturesStep(playerLabel, Position.allPositions))
+            this.add(IncrementAndRestoreManaStep(playerLabel))
+            player.creatures.forEach {
+                if (it.card.cardName in AUTO_HEALERS) {
+                    this.add(HealCreatureStep(playerLabel, it.position, 1))
+                }
+            }
+        }
     }
 }
